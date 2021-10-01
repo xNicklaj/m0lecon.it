e0c9e01fb57eab122a16a05dbf1b0663b939afd0
Simone Aronica <simonearonica@protonmail.ch>
2021-10-01 15:49:04 +0200

    Disabled smooth scrolling on Firefox/Windows

--- main.js
+++ main.js
@@ -1,35 +1,14 @@
-function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
-
-const updateNavbarBackground = debounce(() => {
-  const upperBound = 64;
-  const scrollValue = document.querySelector("html").scrollTop;
-  const navbar = document.querySelector(".navbar");
-  navbar.style = `background: rgba(0,0,0,${scrollValue < upperBound ? scrollValue / upperBound : 1});`
-}, 5);
-
-const sections = document.querySelectorAll("section");
 const html = document.querySelector("html");
 const root = document.querySelector("#root");
-const updateNavbarItems = debounce(() => {
-   
-}, 5);
-
-let isMenuActive = false;
 const navbar = document.querySelector(".navbar");
 const hamburger = document.querySelector("#hamburger");
+let isMenuActive = false;
+const userAgent = navigator.userAgent.toLowerCase();
+
+if(userAgent.indexOf("windows") && userAgent.indexOf("mozilla")){
+  html.style = "scroll-behavior: none";
+}
+
 const toggleMenu = () => {
   if(window.matchMedia("(max-width: 950px)").matches == false) return;
   if(!isMenuActive){
@@ -47,7 +26,5 @@ const toggleMenu = () => {
   isMenuActive = !isMenuActive;
 };
 
-//window.addEventListener('scroll', updateNavbarBackground);
-//window.addEventListener('scroll', updateNavbarItems);
 hamburger.addEventListener('click', toggleMenu);
 document.querySelectorAll(".menu-item").forEach((menuItem) => {menuItem.addEventListener("click", toggleMenu)});
