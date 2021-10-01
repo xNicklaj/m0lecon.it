b8a9cb5ae29784c164d590d60d17935b312b81e8
Simone Aronica <40305701+xNicklaj@users.noreply.github.com>
2021-10-01 16:25:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,12 +5,6 @@ const hamburger = document.querySelector("#hamburger");
 let isMenuActive = false;
 const userAgent = navigator.userAgent.toLowerCase();
 
-/*
-if(userAgent.indexOf("windows") && userAgent.indexOf("mozilla")){
-  html.style = "scroll-behavior: none";
-}
-*/
-
 let scroll = new SmoothScroll('a[data-scroll]', {speed: 600, speedAsDuration: true});
 
 const toggleMenu = () => {
